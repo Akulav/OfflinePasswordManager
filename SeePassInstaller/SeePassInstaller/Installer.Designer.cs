8783b85e8e540f5f1f7b2277b664d2b3563ca4db
Akulav <43496076+Akulav@users.noreply.github.com>
2022-08-14 00:05:33 +0300

    Added Installer to the project (V1.0)

---
+++ SeePassInstaller/SeePassInstaller/Installer.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SeePassInstaller
+{
+    partial class statusdLbl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(statusdLbl));
+            this.installBtn = new System.Windows.Forms.Button();
+            this.uninstallBtn = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // installBtn
+            // 
+            this.installBtn.Location = new System.Drawing.Point(31, 12);
+            this.installBtn.Name = "installBtn";
+            this.installBtn.Size = new System.Drawing.Size(339, 48);
+            this.installBtn.TabIndex = 0;
+            this.installBtn.Text = "Install";
+            this.installBtn.UseVisualStyleBackColor = true;
+            this.installBtn.Click += new System.EventHandler(this.InstallBtn_Click);
+            // 
+            // uninstallBtn
+            // 
+            this.uninstallBtn.Location = new System.Drawing.Point(31, 66);
+            this.uninstallBtn.Name = "uninstallBtn";
+            this.uninstallBtn.Size = new System.Drawing.Size(339, 48);
+            this.uninstallBtn.TabIndex = 1;
+            this.uninstallBtn.Text = "Uninstall";
+            this.uninstallBtn.UseVisualStyleBackColor = true;
+            this.uninstallBtn.Click += new System.EventHandler(this.uninstallBtn_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusLabel.Location = new System.Drawing.Point(0, 119);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(151, 13);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "*Uninstalling will erase all data.";
+            // 
+            // statusdLbl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 132);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.uninstallBtn);
+            this.Controls.Add(this.installBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "statusdLbl";
+            this.Text = "SeePass Installer - Made By Caty V7.1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button installBtn;
+        private System.Windows.Forms.Button uninstallBtn;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ SeePassInstaller/SeePassInstaller/Installer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeePassInstaller
+{
+    public partial class statusdLbl : Form
+    {
+        readonly static string location = "C:\\PasswordManager\\";
+        readonly string fileLocation = "C:\\PasswordManager\\file.zip";
+        public static string[] deletion_list = {
+            location + @"\",
+        };
+
+        public statusdLbl()
+        {
+            InitializeComponent();
+        }
+
+        private void InstallBtn_Click(object sender, EventArgs e)
+        {
+
+            Directory.CreateDirectory(location);
+            
+            File.WriteAllBytes(fileLocation, Properties.Resources.files);
+
+            using (ZipArchive source = ZipFile.Open(fileLocation, ZipArchiveMode.Read, null))
+            {
+                foreach (ZipArchiveEntry entry in source.Entries)
+                {
+                    string fullPath = Path.GetFullPath(Path.Combine(location, entry.FullName));
+
+                    if (Path.GetFileName(fullPath).Length != 0)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+                        // The boolean parameter determines whether an existing file that has the same name as the destination file should be overwritten
+                        entry.ExtractToFile(fullPath, true);
+                    }
+                }
+            }
+            
+            File.Delete(fileLocation);
+
+            IShellLink link = (IShellLink)new ShellLink();
+
+            // setup shortcut information
+            link.SetDescription("My Description");
+            link.SetPath(@"C:\\PasswordManager\\PasswordManager.exe");
+
+            // save it
+            IPersistFile file = (IPersistFile)link;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            file.Save(Path.Combine(desktopPath, "SeePass.lnk"), false);
+
+            statusLabel.Text = "Installation done.";
+            
+        }
+
+        private void uninstallBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                ForceDeleteDirectory(location);
+                Directory.Delete(location, true);
+                File.Delete(Path.Combine(desktopPath, "SeePass.lnk"));
+                statusLabel.Text = "Uninstallation done.";
+            }
+
+            catch
+            {
+                statusLabel.Text = "Some files were not found.";
+            }
+        }
+
+        public static void ForceDeleteDirectory(string path)
+        {
+            var directory = new DirectoryInfo(path) { Attributes = FileAttributes.Normal };
+
+            foreach (var info in directory.GetFileSystemInfos("*", SearchOption.AllDirectories))
+            {
+                info.Attributes = FileAttributes.Normal;
+            }
+        }
+
+
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        internal class ShellLink
+        {
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("000214F9-0000-0000-C000-000000000046")]
+        internal interface IShellLink
+        {
+            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out IntPtr pfd, int fFlags);
+            void GetIDList(out IntPtr ppidl);
+            void SetIDList(IntPtr pidl);
+            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+            void GetHotkey(out short pwHotkey);
+            void SetHotkey(short wHotkey);
+            void GetShowCmd(out int piShowCmd);
+            void SetShowCmd(int iShowCmd);
+            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+            void Resolve(IntPtr hwnd, int fFlags);
+            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+        }
+
+     
+    }
+}
